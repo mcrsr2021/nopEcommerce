b3597a3b678b27968ae332604da2ca26fe22e5f3
mcrsr2021 <mcrsr2021@gmail.com>
2021-09-03 13:24:09 +0530

    new file added and and one file modified

--- src/test/java/com/nopEcommerce/TestCase2.java
+++ src/test/java/com/nopEcommerce/TestCase2.java
@@ -1,2 +1,5 @@
 //This is the new Test Case Crated By Tester 2
 //
+
+
+//This code added newly
---
+++ src/test/java/com/nopEcommerce/TestCase3.java
@@ -0,0 +1,6 @@
+package com.nopEcommerce;
+
+public class TestCase3 {
+
+	//This is newly added Testcase
+}
